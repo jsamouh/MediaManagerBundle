2130316b0dac2761eddac4db1a14b3cb1495cb30
ornicar <thibault.duplessis@gmail.com>
2011-03-15 12:27:59 -0700

    Rename bundle class according to Symfony2 conventions

--- YllyMediaManagerBundle.php
+++ YllyMediaManagerBundle.php
@@ -7,23 +7,23 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Doctrine\DBAL\Types\Type;
 
-class MediaManagerBundle extends Bundle
+class YllyMediaManagerBundle extends Bundle
 {
 	public function boot()
 	{
 		$em = $this->container->get('doctrine.orm.default_entity_manager');
 		Type::addType('longblob', 'Ylly\MediaManagerBundle\Types\LongBlob');
 		$em->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('LONGBLOB', 'longblob');
-		
-		
+
+
 		/*$classLoader = new \Doctrine\Common\ClassLoader('Ylly', __DIR__."/Behavior");
         $classLoader->register();
-        
+
         $evm = $em->getEventManager();
-        
-        
+
+
         $fileableListener = new \Ylly\MediaManagerBundle\Behavior\Fileable\FileableListener();
         $evm->addEventSubscriber($fileableListener);*/
-        
+
 	}
 }
